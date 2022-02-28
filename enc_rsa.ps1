52d64e8cd4e3b62b7750d80bd042cc9d7f697c14
kabutor <43006263+kabutor@users.noreply.github.com>
2022-02-28 22:15:14 +0100

    files

---
+++ enc_rsa.ps1
@@ -0,0 +1,11 @@
+﻿$SourceCertificate = Get-Content -Path '.\kabu_cert.pem' 
+$EncCert=[Convert]::FromBase64String( ($SourceCertificate)[1..($SourceCertificate.Count -2 )] -as [string] )
+$cert = [System.Security.Cryptography.X509Certificates.X509Certificate]::new($EncCert)
+
+$dstStore = New-Object System.Security.Cryptography.X509Certificates.X509Store "My","CurrentUser"
+$dstStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+$dstStore.Add($cert)
+$dstStore.Close
+
+#Get-Childitem -Path Cert:\CurrentUser\My -DocumentEncryptionCert
+#$texto = "test" | Protect-CmsMessage -To C=ES
